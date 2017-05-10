74fc5b7e836757a81edf74d882eeec335ad79d1e
SuperArnold <ji394vul35520@hotmail.com>
2017-05-11 01:03:54 +0800

    Update test.py

--- daemon/unittest/test.py
+++ daemon/unittest/test.py
@@ -1,1 +1,11 @@
-print("HELLO ARNOLD")
+import unittest
+import configparser
+import uuid
+import sys
+sys.path.append('../')
+
+from config import Config
+from rpcservice.dbservice import DBService
+
+config = Config("../xmusic.cfg")
+print(config.db_host)
