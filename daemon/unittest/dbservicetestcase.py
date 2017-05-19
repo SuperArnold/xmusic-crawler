70d0793e8f223ab07a94d1a61e1c5af60ecd971f
SuperArnold <ji394vul35520@hotmail.com>
2017-05-20 00:09:06 +0800

    Update dbservicetestcase.py

--- daemon/unittest/dbservicetestcase.py
+++ daemon/unittest/dbservicetestcase.py
@@ -21,7 +21,7 @@ class DBServiceTestCase(unittest.TestCase):
             config = Config("../xmusic.cfg")
             db_init(config.db_username,
                     config.db_password,
-                    config.db_host,
+                    "localhost",
                     config.db_port,
                     config.db_database)
             print("--------------------------------------")
